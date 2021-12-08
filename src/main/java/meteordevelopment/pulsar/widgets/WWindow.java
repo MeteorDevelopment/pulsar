96fb35ea327fcdc80843cb74511c768be9b465a5
MineGame159 <petulko08@gmail.com>
2021-12-08 17:24:15 +0100

    Windows can now be expanded and collapsed with right click, no button yet

--- src/main/java/meteordevelopment/pulsar/widgets/WWindow.java
+++ src/main/java/meteordevelopment/pulsar/widgets/WWindow.java
@@ -1,20 +1,25 @@
 package meteordevelopment.pulsar.widgets;
 
+import meteordevelopment.pulsar.input.Event;
+import meteordevelopment.pulsar.input.EventType;
 import meteordevelopment.pulsar.input.MouseButtonEvent;
 import meteordevelopment.pulsar.input.MouseMovedEvent;
 import meteordevelopment.pulsar.layout.VerticalLayout;
+import meteordevelopment.pulsar.rendering.Renderer;
 
 import static meteordevelopment.pulsar.utils.Utils.combine;
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_RIGHT;
 
 public class WWindow extends Widget {
     protected static final String[] NAMES = combine(Widget.NAMES, "window");
 
-    private final Widget body;
+    private final Widget header, body;
+    private boolean expanded = true;
 
     private double lastX = -1, lastY;
 
     public WWindow(String title) {
-        Widget header = super.add(new WHeader()).expandX().widget();
+        header = super.add(new WHeader()).expandX().widget();
         header.add(new WText(title).tag("window-title"));
 
         body = super.add(new WBody()).expandX().widget();
@@ -57,10 +62,25 @@ public class WWindow extends Widget {
         lastY = this.y;
     }
 
+    @Override
+    public void dispatch(Event event) {
+        header.dispatch(event);
+        if (expanded) body.dispatch(event);
+
+        if (event.type == EventType.MouseMoved) detectHovered((MouseMovedEvent) event);
+    }
+
+    @Override
+    public void render(Renderer renderer, double delta) {
+        header.render(renderer, delta);
+        if (expanded) body.render(renderer, delta);
+    }
+
     public class WHeader extends Widget {
         protected static final String[] NAMES = combine(Widget.NAMES, "window-header");
 
-        private boolean dragging;
+        private boolean dragging, moved;
+        private boolean shouldToggle;
 
         @Override
         public String[] names() {
@@ -69,19 +89,30 @@ public class WWindow extends Widget {
 
         @Override
         protected void onMousePressed(MouseButtonEvent event) {
+            shouldToggle = false;
+
             if (!event.used && isHovered()) {
                 dragging = true;
+                moved = false;
+                if (event.button == GLFW_MOUSE_BUTTON_RIGHT) shouldToggle = true;
+
                 event.use();
             }
         }
 
         @Override
         protected void onMouseMoved(MouseMovedEvent event) {
-            if (dragging) WWindow.this.move(event.deltaX, event.deltaY);
+            if (dragging) {
+                WWindow.this.move(event.deltaX, event.deltaY);
+                moved = true;
+            }
         }
 
         @Override
         protected void onMouseReleased(MouseButtonEvent event) {
+            if (shouldToggle && !moved)
+                expanded = !expanded;
+
             dragging = false;
         }
     }
